2f999fb9828a58d1b647faa302bb7d7aac14fce3
maxwellaruakpor <36899250+maxwellaruakpor@users.noreply.github.com>
2018-02-27 22:07:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,215 @@
+<?php /*?><?php session_stat();
+?>
+<?php */?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Maxwell AU - Home </title>
+    <!-- Custom styles for this template -->
+    <link href="bower_components/css/app.css" rel="stylesheet">
+  </head>
+
+  <body>
+	<div id="site-wrap">
+		<!-- header divided into 3 parts top header , navbar and hero-welcome -->
+		<header>
+			<section class="top-head">
+				<div class="container">
+					<div class="nav-brand">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					  </button>
+						
+						<a href="index.php"> <img class="img-responsive" src="images/maxwell_au_logo.png" alt="maxwellau-logo"> </a>
+					</div>
+				
+				</div>
+			</section>
+			<nav> 
+				
+			</nav>
+			<div id="navbar" class="collapse navbar-collapse">
+				
+				<div class="container">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="index.php"><i class="fa fa-home"></i> Home</a> </li>
+						<li class="active"><a href="about.php"> About</a> </li>
+						<li class="active"><a href="itinerary.php"> Itinerary</a> </li>
+						<li class="active"><a href="music.php"> Music</a> </li>
+						<li class="active"><a href="contact.php"> Contact</a> </li>
+					</ul>
+					
+					<ul class="navbar navbar-right">
+						<a class="btn btn-default" href="shop.php"> Online Shop </a>
+						<a class="btn btn-default" href="booking.php"> Booking </a>
+					</ul>
+				</div>
+			</div>
+			
+			<section class="welcome">
+				<div class="container">
+					<div class="panel-heading">
+						<div class="container"><h1>WELCOME TO MAXWELL AU SITE</h1></div>
+					</div>
+				</div>
+			</section>
+			
+		</header>
+		
+		<!-- brief about link up to about page-->
+		<section id="brief-abt">
+			<div class="container">
+			<h1> Who Is Maxwell AU?</h1>
+				<p class="lead">Maxwell AU is a creative singer-songwriter, recording artist and a worshipper. He stands amongst the new breeds of worship giants that God has raised in this generation. He has been graced with the ability to blend contemporary praise and worship into African music yet carrying the full meaning and purpose of it. He has been leading his audience into the seasoned presence of God in worship through the help of the Holy Spirit. Maxwell desires always to leave his audience with great satisfaction both spiritually and emotionally. <a class="btn btn-success btn-sm" href="about.php"> Read more... </a> </p>
+			</div>
+		</section>
+		
+		<!-- Current and Previous album or singles-->
+		<section id="discography">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12 col-md-4">
+						<img class="img-responsive" src="images/maxwell au -dependable GOd.jpg">
+						<div class="caption">Dependable GOD Album <a class="btn btn-success btn-sm" href="#"> Learn More</a></div>
+						<iframe src="https://open.spotify.com/embed/artist/2nuUncmBsgPq5iLFQhnn65" width="360" height="380" frameborder="0" allowtransparency="true"></iframe>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<img class="img-responsive" src="images/maxwell au -dependable GOd.jpg">
+						<div class="caption"> Jesu We Ovie  <a class="btn btn-success btn-sm" href="#"> Learn More</a> </div>
+						<iframe src="https://open.spotify.com/embed/artist/2nuUncmBsgPq5iLFQhnn65" width="360" height="380" frameborder="0" allowtransparency="true"></iframe>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<img class="img-responsive" src="images/maxwell au -dependable GOd.jpg">
+						<div class="caption">Shelter Coming Soon <a class="btn btn-success" href="#"> Learn More</a> </div>
+						<iframe src="https://open.spotify.com/embed/artist/2nuUncmBsgPq5iLFQhnn65" width="360" height="380" frameborder="0" allowtransparency="true"></iframe>
+					</div>
+				</div>
+			</div>
+		</section>
+		
+		<!-- Tv Channels that is youTube Channel Videos-->
+		<section id="tv-channel">
+			<div class="container">
+				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+			  <!-- Wrapper for slides -->
+			  <div class="carousel-inner" role="listbox">
+				<div class="item active">
+				
+				  <iframe class="tv-links"  src="https://www.youtube.com/embed/qG07cSxznXA" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				  <div class="carousel-caption">
+					Debut Song from Dependable God Album
+				  </div>
+				</div>
+				<div class="item">
+				  <iframe class="tv-links" src="https://www.youtube.com/embed/FI0-5quJkqg"  allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				  <div class="carousel-caption">
+					Ministering at his home church
+				  </div>
+				</div>
+				
+				<div class="item">
+				  <iframe class="tv-links" src="https://www.youtube.com/embed/9J31bDaNOPI"  allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				  <div class="carousel-caption">
+					Maxwell AU and Friends!
+				  </div>
+				</div>
+				  
+			  </div>
+
+			  <!-- Controls -->
+			  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			 			
+			<!--Thumbnails -->
+					<ul class="thumbnails-carousel clearfix">
+						<li> <img class="img-responsive" src="images/thumbnail-1.jpg"> </li>
+						<li> <img class="img-responsive" src="images/thumbnail-1.jpg"> </li>
+						<li> <img class="img-responsive" src="images/thumbnail-1.jpg"> </li>
+					</ul>
+			</div>
+			
+			</div>
+		</section>
+		
+		<!-- Latest New on Maxwell AU -->
+		<section id="update">
+		</section>
+		
+		<!-- Footer Divided into 4 segments-->
+		<footer id="footer">
+			<div class="container">
+				<div class="row clearfix footer-content">
+					<div class="col-xs-12 col-md-3">
+						<h3>QUICK LINKS</h3>
+						<ul class="list-unstyled">
+							<li><a href="about.php">About</a></li>
+							<li><a href="itinerary.php">Itinerary</a></li>
+							<li><a href="online.php">Online Shop</a></li>
+							<li><a href="booking.php">Booking</a></li>
+							<li><a href="https://www.facebook.com/maxwellaumusic/" target="new">Maxwell AU Facebook</a></li>
+							<li><a href="https://twitter.com/Maxwell_A_U" target="new">Maxwell AU Twitter</a></li>
+							<li><a href="https://www.instagram.com/imaxwellau/" target="new">Maxwell AU Instagram</a></li>
+							<li><a href="https://www.youtube.com/channel/UCj8Fm9zF_pL0DBh9cft4UIQ?view_as=subscriber" target="new">Maxwell AU YouTube</a></li>
+						</ul>
+					</div>
+					<div class="col-xs-12 col-md-3">
+						<h3>TWITTER FEED</h3>
+						<a class="twitter-timeline" data-width="300" data-height="400" data-theme="dark" href="https://twitter.com/Maxwell_A_U?ref_src=twsrc%5Etfw">Tweets by Maxwell_A_U</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+						<a class="twitter-timeline" data-width="300" data-height="400" data-dnt="true" data-theme="dark" href="https://twitter.com/TwitterDev/lists/national-parks?ref_src=twsrc%5Etfw">A Twitter List by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+					</div>
+					<div class="col-xs-12 col-md-3">
+						<h3>INSTAGRAM FEED</h3>
+					
+					</div>
+					<div class="col-xs-12 col-md-3">
+						<h3>CONTACT US</h3>
+					
+					</div>
+				</div>
+			</div>
+			<div class="clearfix footer-copyright">
+				 <!-- .container -->
+				<div class="container">
+				  <!-- .row -->
+				  <div class="row">
+					<div class="col-xs-12">
+					  <p class="text-center">
+						 &copy; <?php echo date("Y")  ?> Maxwell AU Ministries. Designed by <a href="https://hissoundmedia.com">HSM</a>
+					  </p>
+					</div>
+				  </div>
+				  <!-- /.row -->
+				</div>
+				<!-- /.container -->
+
+			</div>
+			
+		</footer>
+		
+		
+		
+	
+	</div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  <script src="bower_components/jquery/dist/jquery.js"></script>
+  <script src="bower_components/jquery/dist/carousel.js"></script>
+ <script src="bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"> </script>
+<script src="bowe_components/bootstrap-sass/assets/javascripts/bootstrap.min.js"></script>
+  </body>
+</html>
